9f24af38cf60d16962d8040b70719a780f18d28a
Matt Henley <thenley28@googlemail.com>
2020-09-07 16:38:01 +0100

    Return a non zero exit status code if curl request fails

--- joke.sh
+++ joke.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "Fetching joke from https://icanhazdadjoke.com"
-echo "SET Joke=$(curl -s -H "Accept: text/plain" https://icanhazdadjoke.com)"
+joke=$(curl -s -H "Accept: text/plain" https://icanhazdadjoke.com)
+if [[ $? != 0 ]]; then
+  echo "Failed to get response from https://icanhazdadjoke.com"
+  exit 1
+else
+  echo "SET Joke=${joke}"
+fi
